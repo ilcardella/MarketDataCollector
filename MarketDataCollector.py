bf305040d6159db3c9256faea4bc991483085f54
alberto <albe.carde@gmail.com>
2018-10-29 23:27:55 +0000

    created structure for new feature

--- MarketDataCollector.py
+++ MarketDataCollector.py
@@ -70,22 +70,37 @@ if __name__ == '__main__':
                 marketId = market['instrument']['marketId']
                 # Convert the string for alpha vantage
                 marketIdAV = '{}:{}'.format(MARKET_COUNTRY, marketId.split('-')[0])
+
                 # Fetch historic price
                 data = get_historic_price(marketIdAV, AV_FUNCTION, AV_INTERVAL, AV_APIKEY)
+
                 # Safety check
                 if 'Error Message' in data or 'Information' in data:
-                    logging.error("Skipping {}".format(marketId))
+                    logging.error("Skipping {}: {}".format(marketId, data))
                     continue
+
                 # Build market folder tree
-                marketFolder = '{}/{}/{}'.format(DB_ROOT_FOLDER, marketId, AV_INTERVAL)
-                os.makedirs(marketFolder, exist_ok=True)
-                # Get timestamp
-                time_str = dt.datetime.now().isoformat()
-                ts = time_str.replace(':', '_').replace('.', '_')
-                # Write on db
-                with open('{}/{}_{}.json'.format(marketFolder, marketId, ts), 'w') as fileWriter:
-                    json.dump(data, fileWriter)
-                    logging.info("Market {} processed succesfully".format(marketId))
+                marketFolder = '{}/{}'.format(DB_ROOT_FOLDER, marketId)
+                marketFilename = '{}_{}.json'.format(marketId, AV_INTERVAL)
+                marketFilepath = '{}/{}'.format(marketFolder, marketFilename)
+
+                # If file exist append new data
+                if os.path.exists(marketFilepath):
+                    with open(marketFilepath, 'r') as fileReader:
+                        market_series = json.load(fileReader)
+                        prices = market_series['Time Series (60min)']
+                        metaData = market_series['Meta Data']
+                        # TODO iter through the series
+                        # Convert time to London tz
+                        # Find the delta between prices and data
+                        # Push front new data
+                        # Write file
+                else:
+                    # Create new file
+                    os.makedirs(marketFolder, exist_ok=True)
+                    with open(marketFilepath, 'w') as fileWriter:
+                        json.dump(data, fileWriter, indent=4, separators=(',', ': '))
+                logging.info("Market {} processed succesfully".format(marketId))
             logging.info("Process complete")
     except Exception as e:
         logging.error(e)
